d02975a39740e43ea3802f270a6480aad1101144
Chen Jr <43334269+Cjr19950214@users.noreply.github.com>
2020-01-03 18:55:52 +0800

    Update jieba_cut.py

--- wiki_word2vec/jieba_cut.py
+++ wiki_word2vec/jieba_cut.py
@@ -2,11 +2,13 @@
 import jieba
 import jieba.analyse
 import codecs
+
+jieba.load_userdict(r'C:\Users\81284\OneDrive\项目！！\1.神经网络情感分析\分词字典处理\merge_wordlist.txt') #添加切词字典
 def cut_words(sentence):
    #print sentence
     return " ".join(jieba.cut(sentence)).encode('utf-8')
-f=codecs.open('wiki.zh.jian.text','r',encoding="utf8")
-target = codecs.open("wiki.zh.jian.seg.txt", 'w',encoding="utf8")
+f=codecs.open('wiki_zh.txt','r',encoding="utf8")
+target = codecs.open("wiki.zh.seg.txt", 'w',encoding="utf8")
 print ('open files')
 line_num=1
 line = f.readline()
